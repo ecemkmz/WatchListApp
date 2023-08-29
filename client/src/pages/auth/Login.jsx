36f37f9f2c0a809cfd7adfc2c0c765c24a33706b
azraagull <azragul85@gmail.com>
2023-08-29 15:54:01 +0300

    control

--- client/src/pages/auth/Login.jsx
+++ client/src/pages/auth/Login.jsx
@@ -25,7 +25,8 @@ const Login = () => {
                   required
                   className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-pink-600 sm:text-sm sm:leading-6"
                 />
-              </div>
+              </div> 
+              
             </div>
 
             <div>
