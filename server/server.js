5a782f79947ed0b3df060074f67078c66cd9e89c
azraagull <azragul85@gmail.com>
2023-09-14 17:38:37 +0300

    14.09.2

---
+++ server/server.js
@@ -0,0 +1,44 @@
+const express = require('express');
+const axios = require('axios');
+
+const app = express();
+const port = 5000; 
+
+
+app.use(express.json());
+const apiKey = 'e7c680bb91msh7cefc06feb84bf0p16346fjsn68ee6f3b768b';
+const host = 'moviesdatabase.p.rapidapi.com';
+const limitPerPage = 50;
+const totalPages = 50;
+for (let page = 1; page <= totalPages; page++) {
+
+app.get(`/get-all-movies/${page}`, async (req, res) => {
+
+    const options = {
+      method: 'GET',
+      url: 'https://moviesdatabase.p.rapidapi.com/actors',
+      params: {
+        page: page,
+        limit: limitPerPage
+      },
+      headers: {
+        'X-RapidAPI-Key': apiKey,
+        'X-RapidAPI-Host': host
+      }
+    };
+
+    try {
+      const response = await axios.request(options);
+      res.json(response.data);
+    } catch (error) {
+      console.error('Hata:', error);
+      return res.status(500).json({ error: 'Veri çekme hatası' });
+    }
+  
+
+
+}); };
+
+app.listen(port, () => {
+  console.log(`Server ${port} portunda çalışıyor.`);
+});
