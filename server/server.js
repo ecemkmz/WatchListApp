3d6697287723c541b29b32bd1bef402b5fd0d16a
azraagull <azragul85@gmail.com>
2023-09-15 11:26:01 +0300

    15.09

--- server/server.js
+++ server/server.js
@@ -1,10 +1,29 @@
 const express = require('express');
+const mongoose = require("mongoose");
+const dotenv = require("dotenv");
 const axios = require('axios');
 
 const app = express();
-const port = 5000; 
+const port = process.env.PORT || 5000; 
+dotenv.config();
+const connect = async () => {
+  try {
+    await mongoose.connect(process.env.MONGO_URI);
+    console.log("Connected to mongoDB");
+  } catch (error) {
+    throw error;
+  }
+};
+const ActorSchema = new mongoose.Schema({
+  nconst: String,
+  primaryName: String,
+  birthYear: String,
+  deathYear: String,
+  primaryProfession: String,
+  knownForTitles: String,
+});
 
-
+const Actors= mongoose.model('Actors', ActorSchema);
 app.use(express.json());
 const apiKey = 'e7c680bb91msh7cefc06feb84bf0p16346fjsn68ee6f3b768b';
 const host = 'moviesdatabase.p.rapidapi.com';
@@ -12,7 +31,7 @@ const limitPerPage = 50;
 const totalPages = 50;
 for (let page = 1; page <= totalPages; page++) {
 
-app.get(`/get-all-movies/${page}`, async (req, res) => {
+app.get(`/actors/${page}`, async (req, res) => {
 
     const options = {
       method: 'GET',
@@ -29,6 +48,8 @@ app.get(`/get-all-movies/${page}`, async (req, res) => {
 
     try {
       const response = await axios.request(options);
+      const actors = response.data.results;
+      await Actors.insertMany(actors);
       res.json(response.data);
     } catch (error) {
       console.error('Hata:', error);
@@ -40,5 +61,6 @@ app.get(`/get-all-movies/${page}`, async (req, res) => {
 }); };
 
 app.listen(port, () => {
+  connect();
   console.log(`Server ${port} portunda çalışıyor.`);
 });
