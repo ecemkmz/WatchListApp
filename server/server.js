479868f0d8d4b142d94803500cf2c7d6c7f1f37e
ecemkmz <ecemk259@gmail.com>
2023-09-15 11:31:54 +0300

    15.09

--- server/server.js
+++ server/server.js
@@ -1,43 +1,38 @@
-const express = require('express');
-const axios = require('axios');
+const express = require("express");
+const axios = require("axios");
 
 const app = express();
-const port = 5000; 
-
+const port = 5500;
 
 app.use(express.json());
-const apiKey = 'e7c680bb91msh7cefc06feb84bf0p16346fjsn68ee6f3b768b';
-const host = 'moviesdatabase.p.rapidapi.com';
+const apiKey = "2cf7318c1amshb225b5248b1194ap182bebjsn49663724f345";
+const host = "moviesdatabase.p.rapidapi.com";
 const limitPerPage = 50;
 const totalPages = 50;
 for (let page = 1; page <= totalPages; page++) {
-
-app.get(`/get-all-movies/${page}`, async (req, res) => {
-
+  app.get(`/get-all-movies/${page}`, async (req, res) => {
     const options = {
-      method: 'GET',
-      url: 'https://moviesdatabase.p.rapidapi.com/actors',
+      method: "GET",
+      url: "https://moviesdatabase.p.rapidapi.com/actors",
       params: {
         page: page,
-        limit: limitPerPage
+        limit: limitPerPage,
       },
       headers: {
-        'X-RapidAPI-Key': apiKey,
-        'X-RapidAPI-Host': host
-      }
+        "X-RapidAPI-Key": apiKey,
+        "X-RapidAPI-Host": host,
+      },
     };
 
     try {
       const response = await axios.request(options);
       res.json(response.data);
     } catch (error) {
-      console.error('Hata:', error);
-      return res.status(500).json({ error: 'Veri çekme hatası' });
+      console.error("Hata:", error);
+      return res.status(500).json({ error: "Veri çekme hatası" });
     }
-  
-
-
-}); };
+  });
+}
 
 app.listen(port, () => {
   console.log(`Server ${port} portunda çalışıyor.`);
